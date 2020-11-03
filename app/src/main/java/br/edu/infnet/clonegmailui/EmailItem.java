a87066f7a5d1de0999378dfab3f9aeb4343b3b5b
duonglegiang <44205911+duonglegiang@users.noreply.github.com>
2020-11-03 23:32:30 +0700

    init cm

---
+++ app/src/main/java/br/edu/infnet/clonegmailui/EmailItem.java
@@ -0,0 +1,97 @@
+package br.edu.infnet.clonegmailui;
+
+
+public class EmailItem {
+
+    private String sender;
+    private String subject;
+    private String brief;
+    private String date;
+    private boolean isImportant = false;
+
+    public EmailItem(String sender, String subject, String brief, String date) {
+        this.sender = sender;
+        this.subject = subject;
+        this.brief = brief;
+        this.date = date;
+    }
+
+    public EmailItem(String sender, String subject, String brief, String date, boolean isImportant) {
+        this.sender = sender;
+        this.subject = subject;
+        this.brief = brief;
+        this.date = date;
+        this.isImportant = isImportant;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public boolean isImportant() {
+        return isImportant;
+    }
+
+    public void setImportant(boolean important) {
+        isImportant = important;
+    }
+
+    public static EmailItem[] samples(){
+        EmailItem[] emails = {
+                new EmailItem("msonline", "Mã kiểm chứng",
+                        "Kiểm chứng email", "2/12/2020"),
+                new EmailItem("Roger", "Quảng cáo",
+                        "Siêu phẩm công nghệ giảm sâu - Coupon đến 5TR!", "2/11/2020"),
+                new EmailItem("Maria", "Quảng cáo",
+                        "Machines can’t actually see, but Computer Vision techniques can bring artificial intelligence into the real world. ", "2/10/2020"),
+                new EmailItem("Lucas", "QC",
+                        "Siêu phẩm công nghệ giảm sâu - Coupon đến 5TR!", "2/9/2020"),
+                new EmailItem("Bernardo", "Thông báo bảo mật",
+                        "Tài khoản Google của bạn vừa được đăng nhập từ thiết bị Windows mới. Bạn nhận được email này vì chúng tôi muốn đảm bảo rằng đó chính là bạn.", "2/8/2020"),
+                new EmailItem("Costa", "Your verification code",
+                        "Your verification code is 245954", "2/7/2020"),
+                new EmailItem("Pedro", "Cảnh báo bảo mật",
+                        "Tài khoản Google của bạn vừa được đăng nhập từ thiết bị Windows mới. Bạn nhận được email này vì chúng tôi muốn đảm bảo rằng đó chính là bạn.", "2/6/2020"),
+                new EmailItem("Luiz", "Cảnh báo bảo mật",
+                        "Tài khoản Google của bạn vừa được đăng nhập từ thiết bị Windows mới. Bạn nhận được email này vì chúng tôi muốn đảm bảo rằng đó chính là bạn.", "2/5/2020"),
+                new EmailItem("Ronaldo", "Cảnh báo bảo mật",
+                        "This is your last chance to complete the 2020 Kaggle Machine Learning and Data Science survey!  The survey will be open for only a few more days! ", "2/4/2020"),
+                new EmailItem("Ronaldo", "Quảng cáo",
+                        "This is your last chance to complete the 2020 Kaggle Machine Learning and Data Science survey!  The survey will be open for only a few more days! ", "12/3/2020"),
+                new EmailItem("Marcos", "Quảng cáo",
+                        "Machines can’t actually see, but Computer Vision techniques can bring artificial intelligence into the real world. ", "2/2/2020"),
+                new EmailItem("Maria", "Quảng cáo",
+                        "Siêu phẩm công nghệ giảm sâu - Coupon đến 3TR!", "2/1/2020"),
+        };
+
+        return emails;
+    }
+}
---
+++ app/src/main/java/br/edu/infnet/clonegmailui/MainActivity.java
@@ -0,0 +1,62 @@
+package br.edu.infnet.clonegmailui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+
+    RecyclerView mRecyclerView;
+    EmailItemAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        mRecyclerView = findViewById(R.id.emailList);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, mLayoutManager.getOrientation()));
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new EmailItemAdapter(EmailItem.samples());
+        mRecyclerView.setAdapter(mAdapter);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.opening_drawer, R.string.closing_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
